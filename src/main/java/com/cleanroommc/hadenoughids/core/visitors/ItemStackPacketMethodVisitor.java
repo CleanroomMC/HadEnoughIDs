b46ae7229d9998ae24fe5a5aeeca42c211f62d6b
Rongmario <rongmario@protonmail.com>
2022-04-03 22:05:32 +0100

    Patch every entry of read/writeShort in write/readItemStack

--- src/main/java/com/cleanroommc/hadenoughids/core/visitors/ItemStackPacketMethodVisitor.java
+++ src/main/java/com/cleanroommc/hadenoughids/core/visitors/ItemStackPacketMethodVisitor.java
@@ -5,25 +5,18 @@ import org.objectweb.asm.Opcodes;
 
 public class ItemStackPacketMethodVisitor extends MethodVisitor implements Opcodes {
 
-    private boolean prepareToPatch = false;
-
     public ItemStackPacketMethodVisitor(MethodVisitor methodVisitor) {
         super(ASM5, methodVisitor);
     }
 
     @Override
     public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-        if (!prepareToPatch && ("writeByte".equals(name) || "readByte".equals(name))) {
-            prepareToPatch = true;
-        } else if (prepareToPatch) {
-            if ("writeShort".equals(name)) {
-                prepareToPatch = false;
-                super.visitMethodInsn(opcode, owner, "writeInteger", desc, itf);
-                return;
-            } else if ("readShort".equals(name)) {
-                super.visitMethodInsn(opcode, owner, "readInteger", "()I", itf);
-                return;
-            }
+        if ("writeShort".equals(name)) {
+            super.visitMethodInsn(opcode, owner, "writeInteger", desc, itf);
+            return;
+        } else if ("readShort".equals(name)) {
+            super.visitMethodInsn(opcode, owner, "readInteger", "()I", itf);
+            return;
         }
         super.visitMethodInsn(opcode, owner, name, desc, itf);
     }
